ff20f12b65f98b5b26a8e988fbf818df4d799257
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-03 12:03:03 +0300

    feat: add solution to problem create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import fs, { readdir } from 'fs/promises';
+import path from 'path';
+
 export const create = async () => {
-    // Write your code here 
+  const __dirname = path.join(path.dirname(''), 'files');
+  const files = await readdir(__dirname);
+  for (const file of files) {
+    if (file === 'fresh.txt') {
+      throw new Error('FS operation failed')
+    }
+  }
+  fs.writeFile(
+      path.join(__dirname, 'fresh.txt'),
+      'I am fresh and young',
+  );
 };
+
+await create();
