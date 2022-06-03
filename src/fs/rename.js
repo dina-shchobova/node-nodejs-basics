f403c49563c09925f17941ec1b46d01dfb1a676e
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-03 12:26:40 +0300

    feat: add solution to problem rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,25 @@
+import fs, { readdir } from 'fs/promises';
+import path from 'path';
+
 export const rename = async () => {
-    // Write your code here 
+  let wrongFileExists = false;
+  let properFileExists = false;
+  const files = await readdir(path.join(path.dirname(''), 'files'));
+  for (const file of files) {
+    if (file === 'properFilename.txt') {
+      properFileExists = true;
+      break;
+    }
+    if (file === 'wrongFilename.txt') {
+      wrongFileExists = true;
+      const oldName = path.join(path.dirname(''), 'files', 'wrongFilename.txt');
+      const newName = path.join(path.dirname(''), 'files', 'properFilename.txt');
+      fs.rename(oldName, newName);
+    }
+  }
+  if (wrongFileExists === false || properFileExists === true) {
+    throw new Error('FS operation failed');
+  }
 };
+
+await rename();
