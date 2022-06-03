96d4c28606e5a1a094e16887a0425f85a1689fae
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-03 14:46:08 +0300

    feat: add solution to problem list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,14 @@
+import { readdir, access } from 'fs/promises';
+import path from 'path';
+
 export const list = async () => {
-    // Write your code here 
+  try {
+    await access(path.join(path.dirname(''), 'files'));
+    const files = await readdir(path.join(path.dirname(''), 'files'));
+    console.log(files);
+  } catch {
+    console.log('FS operation failed');
+  }
 };
+
+await list();
