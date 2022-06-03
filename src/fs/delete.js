3f86aebcff00e37eb7a386f3e07839748018622f
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-03 12:30:16 +0300

    feat: add solution to problem delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,19 @@
+import { readdir, rm } from 'fs/promises';
+import path from 'path';
+
 export const remove = async () => {
-    // Write your code here 
+  let fileWasFound = false;
+  const files = await readdir(path.join(path.dirname(''), 'files'));
+  for (const file of files) {
+    if (file === 'fileToRemove.txt') {
+      fileWasFound = true;
+      rm(path.join(path.dirname(''), 'files', 'fileToRemove.txt'));
+      break;
+    }
+  }
+  if (fileWasFound === false) {
+    throw new Error('FS operation failed');
+  }
 };
+
+await remove();
