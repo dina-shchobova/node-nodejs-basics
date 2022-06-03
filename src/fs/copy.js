0f129e512ad56535c2ad5bd7864aa3df90c2ebc5
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-03 12:10:06 +0300

    feat: add solution to problem copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,28 @@
+import fs, { readdir } from 'fs/promises';
+import path from 'path';
+
 export const copy = async () => {
-    // Write your code here 
+  const files = await readdir(path.dirname(''));
+  for (const file of files) {
+    if (file === 'files_copy' && !file.isFile) {
+      throw new Error('FS operation failed')
+    }
+  }
+
+  fs.mkdir(path.join(path.dirname(''), 'files_copy'), err => {
+    if (err) throw err;
+  });
+
+  const originFiles = await readdir(path.join(path.dirname(''), 'files'));
+  for (const file of originFiles) {
+    try {
+      const from = path.join(path.dirname(''), 'files', `${file}`);
+      const to = path.join(path.dirname(''), 'files_copy', `${file}`);
+      await fs.copyFile(from, to);
+    } catch {
+      throw new Error('FS operation failed 2');
+    }
+  }
 };
+
+await copy();
