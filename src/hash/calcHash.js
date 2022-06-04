6be828475753c16a76cd0b85dff7943a06e22b04
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-04 15:51:05 +0300

    feat: add solution to problem calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,13 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { createHash } from 'crypto';
+import { stdout } from 'process';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+
+  const input = createReadStream(path.join(path.dirname(''), 'files', 'fileToCalculateHashFor.txt'));
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
+
+calculateHash();
