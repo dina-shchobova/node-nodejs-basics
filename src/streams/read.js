e2209e95a044ac9927df261f85f0a07e877bed2e
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-04 17:12:54 +0300

    feat: add solution to problem read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,11 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+  let data = '';
+  const readStream = createReadStream(path.join(path.dirname(''), 'files', 'fileToRead.txt'), 'utf-8');
+  readStream.on('data', chunk => data += chunk);
+  readStream.on('end', () => process.stdout.write(data));
 };
+
+read();
