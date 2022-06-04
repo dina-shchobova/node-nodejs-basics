41cf735f4291ebf21e0f533f21e260e270f2b774
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-05 02:03:49 +0300

    fix: fix solution to problem decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,12 +1,18 @@
 import { createWriteStream, createReadStream } from 'fs';
+import { access } from 'fs/promises';
 import path from 'path';
 import zlib from 'zlib';
 
 export const decompress = async () => {
-  const readStream = createReadStream(path.join(path.dirname(''), 'files', 'archive.gz'));
-  const writeStream = createWriteStream(path.join(path.dirname(''), 'files', 'fileToCompress.txt'));
-  let unzip = zlib.createUnzip();
-  readStream.pipe(unzip).pipe(writeStream);
+  try {
+    await access(path.join(path.dirname(''), 'files', 'archive.gz'));
+    const readStream = createReadStream(path.join(path.dirname(''), 'files', 'archive.gz'));
+    const writeStream = createWriteStream(path.join(path.dirname(''), 'files', 'fileToCompress.txt'));
+    let unzip = zlib.createUnzip();
+    readStream.pipe(unzip).pipe(writeStream);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 decompress();
