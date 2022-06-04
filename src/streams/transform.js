3343e6322ea00e36cfdc27aac7c1283a122a91e7
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-05 01:18:05 +0300

    feat: add solution to problem transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,20 @@
+import { Transform } from "stream";
+import { stdin, stdout } from 'process';
+
 export const transform = async () => {
-    // Write your code here 
+  stdout.write('Write some text and press ENTER \n');
+
+  const reverseStream = () => {
+    return new Transform({
+      transform(chunk, encoding, callback) {
+        const reverseStr = chunk.toString().split('').reverse().join('');
+        callback(null, reverseStr);
+      }
+    });
+  };
+
+  const reverse = reverseStream();
+  stdin.pipe(reverse).pipe(stdout);
 };
+
+transform();
