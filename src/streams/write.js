4988612a2c0ec45a357f2d83c9db4773d18b635f
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-04 18:00:10 +0300

    feat: add solution to problem write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import { createWriteStream } from 'fs';
+import { stdin, stdout } from 'process';
+import path from 'path';
+
 export const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(path.join(path.dirname(''), 'files', 'fileToWrite.txt'));
+  stdout.write('Write some text and press ENTER \n');
+
+  stdin.on('data', data => {
+    const value = data.toString();
+    writeStream.write(value);
+    process.exit();
+  });
+
 };
+
+write();
