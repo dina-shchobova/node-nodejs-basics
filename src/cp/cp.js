07c73ed352c9b04955eb9389e1d445126e4c7622
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-05 19:04:01 +0300

    feat: add solution to problem cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,35 @@
+import {spawn} from 'child_process';
+import {stdin, stdout} from 'process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const subprocess = spawn('node', ['./files/script.js', ...args]);
+
+  subprocess.stdout.setEncoding('utf8');
+  subprocess.stdout.on('data', function (data) {
+    console.log(data.toString());
+  });
+
+  subprocess.stderr.on('data', function (data) {
+    console.log(data.toString());
+  });
+
+  subprocess.on('error', console.log)
+
+  subprocess.stdin.setEncoding('utf8');
+
+  subprocess.on('close', function (code) {
+    console.log('process exit code ' + code);
+    process.exit();
+  });
+
+  stdout.write('Write some text and press ENTER. Write CLOSE to exit. \n');
+
+  stdin.on('data', data => {
+    const value = data.toString();
+    subprocess.stdin.write(value);
+  });
+
+
 };
+
+await spawnChildProcess(['--uilo', 'utin']);
