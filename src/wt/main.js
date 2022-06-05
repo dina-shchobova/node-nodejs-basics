6eefa3015ebebea85ff34084e79de8f32d53629d
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-05 13:36:02 +0300

    feat: add solution to problem wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,27 @@
+import {Worker} from 'worker_threads';
+import {cpus} from "os";
+
 export const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = cpus().length;
+  let promises = [];
+  for (let i = 0; i < numCPUs; i++) {
+    const worker = new Worker('./worker.js', {
+      workerData: i + 10,
+    });
+    const promise = new Promise((resolve) => {
+      worker.on('message', msg => {
+        if (msg instanceof Error) {
+          resolve({'status': 'error', 'data': null})
+        } else {
+          resolve({'status': 'resolved', 'data': msg})
+        }
+      });
+    })
+    promises.push(promise);
+  }
+  return await Promise.all(promises).then(results => {
+    console.log(results);
+  })
 };
+
+performCalculations();
